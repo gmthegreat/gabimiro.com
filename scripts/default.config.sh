427261f21562100e4f11b3978e51b177ce4d57cf
YaronMiro <yaron@gizra.com>
2016-01-30 01:33:08 +0200

    wip: Adding import script

---
+++ scripts/default.config.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+#########################################################################################
+#
+# Import used in the different scripts.
+#
+# Copy this file in the same directory, the filename of the copy should be "config.sh".
+#
+#########################################################################################
+
+
+# Server authorized user name.
+USER_NAME="gabriel61"
+# Server name.
+SERVER_NAME="gabrielmiro.com"
+# Port.
+PORT="2222"
---
+++ scripts/default.import.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Load the config file.
+source config.sh
+
+
+echo 
+
+
+ssh "$USER_NAME"@$SERVER_NAME -p $PORT"
